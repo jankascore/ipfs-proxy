48b68aa2054d0a526b560dec64f0d254f05ec627
McKittrick Kaminski <mckittrick.kaminski@gmail.com>
2023-02-26 15:11:28 -0600

    basic proxy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import { cat } from './ipfs.js';
 
 export const app = express();
 app.use(morgan('common'));
 
 app.get('/ipfs/:cid', async (req, res) => {
-	setTimeout(() => res.send('hi'), 1000)
+	try {
+		res.send(await cat(req.params.cid))
+	} catch(e) {
+		console.error(e);
+	}
+})
+
+app.listen(8080, () => {
+	console.log('Listening on 8080');
 })
