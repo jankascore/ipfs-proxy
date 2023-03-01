10121ee742191c3d6e2bf447230f2b6e2e138742
McKittrick Kaminski <mckittrick.kaminski@gmail.com>
2023-03-01 04:15:25 -0600

    nothing of substance

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -2,9 +2,9 @@ import {create} from 'ipfs-http-client';
 import {config} from 'dotenv';
 config();
 
-const ipfs = create({url: process.env.IPFS_URL!})
 
 export const cat = async (cid: string): Promise<string> => {
+	const ipfs = create({url: process.env.IPFS_URL!})
 	const stream = ipfs.cat(cid, {timeout: 10000}); // 10s timeout
 	const decoder = new TextDecoder()
 
